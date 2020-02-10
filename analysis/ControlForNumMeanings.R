6d7f188456012dbe24273eb00b0d76f31cdb0034
S G Roberts <sgroberts@v3010-0a83073b.wifi.cf.ac.uk>
2020-02-10 17:19:05 +0000

    add CLICS num meanings

---
+++ analysis/ControlForNumMeanings.R
@@ -0,0 +1,16 @@
+try(setwd("~/Documents/Bristol/word2vec/word2vec_DPLACE/analysis/"))
+library(dplyr)
+d = read.csv("../data/FAIR/nel-wiki-k100-alignments-merged-long.csv",
+             stringsAsFactors = F,
+             encoding = "UTF-8",fileEncoding = "UTF-8")
+
+m = read.csv("../../offline/CLICS_numMeanings.csv")
+
+d = left_join(d,m,by = c("Glottocode_l1"="glottocode","Word_Form_l1"="sourceForm"))
+names(m) = paste0(names(m),".l2")
+d = left_join(d,m,by = c("Glottocode_l2"="glottocode.l2","Word_Form_l2"="sourceForm.l2"))
+
+d = d[complete.cases(d[,c("NumMeanings","NumMeanings.l2")]),]
+
+
+summary(lm(local_alignment ~ NumMeanings, data=d))
